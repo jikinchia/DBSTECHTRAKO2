128613237d225add4d1d1372551514b19ebe2434
Unknown <jkchia.2015@sis.smu.edu.sg>
2019-02-01 11:23:48 +0800

    DBS.Web

---
+++ DBS.Web/src/main/java/com/dbs/techtrako2/DBS/Web/Application.java
@@ -0,0 +1,14 @@
+package com.dbs.techtrako2.DBS.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
